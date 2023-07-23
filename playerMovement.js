9d8ae5e1f139c50add4dd3b8cc4be5c789a30004
lewiscode28 <95774954+lewiscode28@users.noreply.github.com>
2023-07-23 19:00:50 +0100

    Add files via upload

---
+++ playerMovement.js
@@ -0,0 +1,23 @@
+AFRAME.registerComponent("player-movement", {
+  init: function () {
+    this.walk();
+  },
+  walk: function () {
+    window.addEventListener("keydown", (e) => {
+    //add the code to play sound as per selection of arrow keys for movement
+    if (
+      e.key === "ArrowUp" ||
+      e.key === "ArrowRight" ||
+      e.key === "ArrowLeft" ||
+      e.key === "ArrowDown"
+    ) {
+      var entity = document.querySelector("#sound2");
+      entity.components.sound.playSound();
+    }
+
+
+
+    });
+  },
+});
+
